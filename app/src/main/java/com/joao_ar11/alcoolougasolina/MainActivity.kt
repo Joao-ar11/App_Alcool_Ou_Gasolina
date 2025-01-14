24705c6f226f0d19d77c7f8475655334a04dbf4d
João_A.11 <jarlpereira29@gmail.com>
2025-01-14 16:17:34 -0300

    Adiciona componentes de interface a MainActivity

--- app/src/main/java/com/joao_ar11/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/joao_ar11/alcoolougasolina/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.joao_ar11.alcoolougasolina
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textInputAlcool: TextInputLayout
+    private lateinit var editAlcool: TextInputEditText
+
+    private lateinit var textInputGasolina: TextInputLayout
+    private lateinit var editGasolina: TextInputEditText
+
+    private lateinit var botaoCalcular: Button
+    private lateinit var textResultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inicializarComponentesInterface()
+    }
+
+    private fun inicializarComponentesInterface() {
+        textInputAlcool = findViewById(R.id.text_input_alcool)
+        editAlcool = findViewById(R.id.edit_alcool)
+
+        textInputGasolina = findViewById(R.id.text_input_gasolina)
+        editGasolina = findViewById(R.id.edit_gasolina)
+
+        botaoCalcular = findViewById(R.id.botao_calcular)
+        textResultado = findViewById(R.id.text_resultado)
     }
 }
